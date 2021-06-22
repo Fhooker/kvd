68f2ee5f62bbcc85836d4a00743d1e9c73d69464
sanbo <sanbo.xyz@gmail.com>
2021-06-22 19:09:35 +0800

    update

--- app/src/main/java/me/keepalive/demo/rs/acc/SyncService.java
+++ app/src/main/java/me/keepalive/demo/rs/acc/SyncService.java
@@ -35,7 +35,6 @@ public class SyncService extends Service {
 
         @Override
         public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-
         }
     }
 }
